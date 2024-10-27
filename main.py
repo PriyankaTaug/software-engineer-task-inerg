1934c0b407ec4c2552fce55238d37d033a28c82f
unknown <aghilafalgunana@gmail.com>
2024-10-27 16:43:17 +0530

    second commit

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 
 def read_excel_data():
     df = pd.read_excel('20210309_2020_1 - 4 (1) (1) (1) (1).xls')
-    app_data = df.groupby('API WELL  NUMBER').agg({'OIL':'sum','GAS':'sum','BRINE':'sum'})
+    app_data = df.groupby('API WELL  NUMBER').agg({'OIL':'sum','GAS':'sum','BRINE':'sum'}).reset_index()
     
     # DB connecting
     conn = sqlite3.connect('Ohio.db')
     app_data.to_sql('quarterly_productiontbl',conn,if_exists='replace',index=False)
     conn.close()
+
 
 
    
